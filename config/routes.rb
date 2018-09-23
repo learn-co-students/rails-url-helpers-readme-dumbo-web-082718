8f42cafc35691b1cdfdfeab665815153b1326f92
Malcome Fein <mfein90@gmail.com>
2018-09-23 10:56:13 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
+  # this will create *****routing methods****** for posts that we can utilize in our views and controllers.
+      get '/register', to: 'users#new', as: 'register'
 end
